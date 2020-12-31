294f472a642cbecbc969cdf60072a7640494d512
Sultan Ullah <sultan.ullah7@gmail.com>
2020-12-31 10:57:19 -0800

    Added basic routes for books API

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,33 @@ import * as express from 'express';
 
 (async () => {
   try {
-    const app = express()
-    const port = 3000
+    const app = express();
+    const port = 3000;
+    const router = express.Router();
 
-    app.get('/', (req, res) => {
-      res.send('Hello World!')
-    });
-
-    app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`)
+    router.get('/books', function (req, res) {
+      res.send('All books')
     })
+
+    router.get('/books/:id', function (req, res) {
+      res.send('One book')
+    })
+
+    router.post('/books', function (req, res) {
+      res.send('Create book');
+    })
+
+    router.delete('/books/:id', function (req, res) {
+      res.send('Delete a book');
+    })
+
+    router.put('/books/:id', function (req, res) {
+      res.send('Update a book');
+    })
+
+    app.use(router);
+    app.listen(port, () => console.log(`Express is listening on port: ${port}`))
+    
   } catch (error) {
     console.log(error);
   }
